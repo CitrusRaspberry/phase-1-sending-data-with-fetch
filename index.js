5082de6b4d7351f032765c2664c53f095c200080
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-06 20:00:13 -0500

    commit tree 742aaa44e1b16bad838aa841cdda7eccce450b3c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+function submitData(name, email) {
+  const body = document.querySelector("body");
+
+
+  const submittedData = {
+    name: name,
+    email: email
+  };
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(submittedData)
+  };
+
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then( resp => resp.json() )
+  .then( json => {
+    console.log(json);
+    body.append(json.id);
+  })
+  .catch( error => {
+    alert("Bad JUJU");
+    console.log(error);
+    body.append(error);
+  });
+}
+
+function getData(id) {
+  return fetch(`http://localhost:3000/users/${id}`)
+  .then( resp => resp.json() )
+  .then( json => console.log(json))
+  .catch( error => console.log(error));
+}
